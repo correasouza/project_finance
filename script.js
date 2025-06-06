6927d23909c43f023167027a540198a9d2ab4198
igor.correa <igor.correa@unifesspa.edu.br>
2025-06-06 15:32:21 -0300

    finalizando html e css da tela inicial

---
+++ script.js
@@ -0,0 +1,7 @@
+
+<script>
+document.getElementById('botao-simular').addEventListener('click', function() {
+    window.open('pagina-nova.html', '_blank'); // Abre em nova aba
+    // ou use window.location.href = 'pagina-nova.html'; para abrir na mesma aba
+});
+</script>
